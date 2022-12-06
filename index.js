d64fe3046166ad86cfe4b1efd55692e450d7fca6
yangrangrang <gkssk2309@gmail.com>
2022-12-06 15:48:40 +0900

    몰라이상해

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ alert("안녕!");
 
 document.querySelector("h1").addEventListener("mouseover",e){
     e.target.style.color = "blue";
-}
+};
 // h1을 Selector로 선택한 이유는 암묵적으로 h1은 하나만 쓰게 되어있다.
 // 의미론적 태그요소로 도큐먼트 전체의 제목이다 (검색엔진이 그런 의미를 갖는다)
 
